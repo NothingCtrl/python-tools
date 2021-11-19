10473e8d6c3f1c02ee4b226cebc2c3512daf4d81
NothingCtrl <nothingctrl@gmail.com>
2021-11-19 13:58:18 +0700

    Add login_to_pod.py

--- README.md
+++ README.md
@@ -15,4 +15,10 @@ If a build file (executable file) exist, the OS using to build file is:
 * help: `python3 file_split.py -h`
 * split file (default mode): `python3 file_split.py -i path_of_input_file -o path_of_output_dir`
     * Set split file size: default is `104857600 bytes` (100MB), use `--size \ -s` to change this value
-* join split files: `python3 file_split.py -m join -i path_of_first_splited_file -o path_of_join_file`  
+* join split files: `python3 file_split.py -m join -i path_of_first_splited_file -o path_of_join_file`
+
+### `login_to_pod.py`: help tool find and login to kubernetes pod
+
+* help: `python3 login_to_pod.py -h`
+* usage: `python3 login_to_pod.py -p pod-name`
+    * The `pod-name` is not need exactly match
---
+++ kubernetes/login_to_pod.py
@@ -0,0 +1,34 @@
+"""
+Help tool to find a kubernetes pod with specific name then login
+This required current user environment already setup google-cloud-sdk with kubectl component
+"""
+import os
+import sys
+import argparse
+
+if getattr(sys, 'frozen', False):
+    # frozen
+    from sys import exit
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--pod", "-p", help="Kubernetes pod name")
+parser.add_argument("--shell", "-s", help="Pod shell, default: /bin/bash", default="/bin/bash")
+
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    if not args.pod:
+        print("Please input pod name with --pod/-p")
+        exit()
+    pods_list = os.popen("kubectl get pods").read()
+    pod_name = None
+    for line in pods_list.splitlines():
+        if args.pod in line and "Running" in line:
+            pod_name = line.split(' ')[0]
+            print(f"Found pod name: {pod_name}")
+            break
+    if pod_name:
+        os.system(f"start cmd /k kubectl exec --stdin --tty {pod_name} -- {args.shell}")
+    else:
+        print(f"Cannot find pod with name: {pod_name}")
