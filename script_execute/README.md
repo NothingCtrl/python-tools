f8330b158e01a1d5cb8a01c88cc926d725d37520
Duong Bao Thang <thangdb@solarbk.vn>
2023-04-14 16:46:03 +0700

    [FEA] script_execute: support single script execute

--- script_execute/README.md
+++ script_execute/README.md
@@ -1,14 +1,15 @@
 ## Script Execute
 
-Using to execute all script file in `scripts` folder and get output
+Using to execute all script files in the `scripts` folder and get output, the script must be fully automatic without any user interaction 
 
 ### Usage:
 
 * Create folder `scripts` in same location
 * Add your script file(s) to `scipts` folder, only execute file with extension(s): `sh, bat, ps1`
 * Run app: 
-    * run with no timeout: `python script_execute.py`
-    * with timeout, example for 30 seconds: `python script_execute.py 30`
+    * execute all script(s) with no timeout: `python script_execute.py`
+    * execute all script(s) with timeout for 30 seconds: `python script_execute.py 30`
+    * execute single script with no timeout: `python script_execute.py 0 my_script.sh`
 * The output logs will save in `logs` folder
 
 ### Build app:
--- script_execute/script_execute.py
+++ script_execute/script_execute.py
@@ -65,15 +65,26 @@ if __name__ == "__main__":
 
     print(f"=== Execute file with extension: {', '.join(ALLOW_EXTENSION)} ===")
 
-    _timeout = None
+    _timeout, execute_script = None, None
     if len(sys.argv) >= 2:
         if str.isdecimal(sys.argv[1]) and int(sys.argv[1]) > 0:
             _timeout = int(sys.argv[1])
+        if len(sys.argv) >= 3:
+            execute_script = sys.argv[2]
+
     print(f"=== Execute with timeout set (seconds): {_timeout if _timeout else 'un-limit'} ===")
+    if execute_script:
+        print(f"=== Single script execute: {execute_script}")
 
     scripts = []
     if os.path.isdir(script_dir):
         scripts = os.listdir(script_dir)
+        if execute_script is not None:
+            if execute_script in scripts:
+                scripts = [execute_script]
+            else:
+                print(f"  - Script is not found in scripts folder: {script_dir}")
+                scripts = []
         for item in scripts:
             if os.path.isfile(os.path.join(script_dir, item)) and item.split('.')[-1].lower() in ALLOW_EXTENSION:
                 thread_list.append({
@@ -81,6 +92,8 @@ if __name__ == "__main__":
                     'thread': run_script(os.path.join(script_dir, item))
                 })
         wait_to_finish(_timeout)
+    else:
+        os.mkdir(script_dir)
     log_dir = os.path.join(base_dir, 'logs')
     if not os.path.isdir(log_dir):
         os.mkdir(log_dir)
@@ -97,6 +110,7 @@ if __name__ == "__main__":
     with open(os.path.join(log_dir, log_file), 'w+', errors='ignore') as f:
         f.write(f"Report for date: {time_now.strftime('%Y:%m:%d %H:%M:%S')}\n")
         f.write(f"Execute timeout: {_timeout if _timeout else 'no timeout'}\n")
+        f.write(f"Single script execute mode: {'YES' if execute_script else 'NO'}\n")
         f.write(f"Total script: {len(scripts)}\n")
         f.write(f"Total response: {count_exe}\n")
         f.write("============\n")
